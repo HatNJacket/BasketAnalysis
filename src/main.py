526ee31242009352211545eee90ab9c48d0cc194
HatNJacket <nickdrapak@yahoo.ca>
2025-05-14 16:19:52 -0400

    First Commit

---
+++ src/main.py
@@ -0,0 +1,8 @@
+""" TODO:
+- Read data from text or .csv, same function
+- Do Apriori (Stop at maybe 2000? Takes 5 seconds in theory)
+- Do PCY
+- Build a graph
+- Set up an html homepage with my dataset already uploaded
+- Anything more is just a happy extra bit. Think about all this after the website is set up and displaying graphs.
+"""
